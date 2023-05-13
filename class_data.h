be2d62807518ce1e57159ff2b329e99660e205fc
AshirArfan <u2022124@giki.edu.pk>
2023-05-13 20:37:35 +0500

    class data commit 1

---
+++ class_data.h
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class User;
+class Member;
+class Items;
+struct Requests;
+
+class User
+{
+    protected:
+        string first_name;
+        string last_name;
+        int age;
+    
+};
+
+class Member : public User
+{
+    public:
+        string username,password,req;
+    
+};
+
+class Items
+{
+    public:
+        string itemname;
+        int quantity;
+
+        void item_info()
+        {
+            cout<<"----------------------------\n";
+            cout<<"Item Name: "<<itemname<<endl;
+            cout<<"Quantity: "<<quantity<<endl;
+            cout<<"----------------------------\n";
+        }
+};
+
+struct Requests
+{
+    string username;
+    string request;
+};
---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include"class_data.h"
+using namespace std;
+
+string AdminPass="abc12345";
+
+int main()
+{
+    Member memberreq[20];
+    Member members[50];
+    Items item[50];
+    Requests item_reqs[10];
+
+    int num_members=0;
+    int num_items=0;
+    int user_reqs=0;
+
+
+    cout << "\t\tWelcome to ABC's Inventory\n";
+    cout<<"\n\nLogin as: ";
+    cout <<"\n1. Admin.\n2. Member\n3. Exit";
+    cout<<"Enter: ";
+    
+    int option;
+    cin>>option;
+
+    switch (option)
+    {
+    case 1:
+        {
+            cout<<"Enter Pasword: ";
+            string pass;
+            cin>>pass;
+
+            if (pass==AdminPass)
+            {
+                cout<<"Welcome to Admin Pannel\n";
+                cout<<"\n1. Check new members requests.\n";
+                cout<<"2. Remove a member.\n";
+                cout<<"3. Check Inventory.\n";
+                cout<<"4. Restock Inventory.\n";
+                cout<<"5. Check Requests.\n";
+                cout<<"6. Change Admin Password.\n";
+                cout<<"6. Exit.\n";
+            }
+        }
+        break;
+    
+    default:
+        break;
+    }
+}
