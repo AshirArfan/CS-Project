129600228e76c56dcb7c6a42ebbafcb965e4b883
AshirArfan <u2022124@giki.edu.pk>
2023-05-14 00:28:48 +0500

    final admin data commit

--- class_data.h
+++ class_data.h
@@ -5,7 +5,11 @@ using namespace std;
 class User;
 class Member;
 class Items;
+class PhysicalItems;
+class SoftwareItems;
 struct Requests;
+struct Itemstaken;
+
 
 class User
 {
@@ -19,6 +23,24 @@ class Member : public User
 {
     public:
         string username,password,req;
+        Itemstaken itmstaken[50];
+        int num_items_taken;
+
+        void get_info()
+        {
+            cout<<"Enter your full name: ";
+            getline(cin,full_name);
+            cout<<"Enter age: ";
+            cin>>age;
+        }
+
+        void show_info()
+        {
+            cout<<"\n------------------------\n";
+            cout<<"Name: "<<full_name<<endl;
+            cout<<"Username: "<<username<<endl;
+            cout<<"------------------------\n";
+        }
     
 };
 
@@ -27,12 +49,34 @@ class Items
     public:
         string itemname;
         int quantity;
+        virtual void item_info()=0;
 
+};
+
+class PhysicalItems : public Items
+{
+    public:
         void item_info()
         {
             cout<<"----------------------------\n";
             cout<<"Item Name: "<<itemname<<endl;
             cout<<"Quantity: "<<quantity<<endl;
+            if (quantity<=10)
+                cout<<"Quantity running low!\n";
+            cout<<"----------------------------\n";
+        }
+};
+
+class SoftwareItems : public Items
+{
+    public:
+        void item_info()
+        {
+            cout<<"----------------------------\n";
+            cout<<"Item Name: "<<itemname<<endl;
+            cout<<"Quantity: "<<quantity<<endl;
+            if (quantity<=20)
+                cout<<"Quantity running low!\n";
             cout<<"----------------------------\n";
         }
 };
@@ -41,4 +85,10 @@ struct Requests
 {
     string username;
     string request;
+};
+
+struct Itemstaken
+{
+    string item_name;
+    int quantity;
 };
